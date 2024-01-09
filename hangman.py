fe54433bdd58988bb4dbcf4a332a619527c5f019
Manoj Kumar <ms8460149@gmail.com>
2024-01-09 16:24:11 +0530

    feat:shows game report

--- hangman.py
+++ hangman.py
@@ -89,6 +89,18 @@ print(logo)
 
 blanks = []
 lives = len(stages)
+score = 0
+total_attempts = 0
+wrong_attempts = 0
+right_attempts = 0
+
+
+def show_result():
+    print(
+        f"Score: {score}\nAccuracy: {round((right_attempts/total_attempts)*100,2)}%\nTotal attempts: {total_attempts}\nRight Attempts: {right_attempts}\nWrong Attempts: {wrong_attempts}"
+    )
+
+
 for blank in range(len(chosen_word)):
     blanks += "_"
 
@@ -98,18 +110,25 @@ while "".join(blanks) != chosen_word and lives > 0:
     guess = input("Guess a letter in word? ").lower()
     is_correct = False
     index = 0
+    total_attempts += 1
     for letter in chosen_word:
         if letter == guess:
             # Replace blanks with letter
             blanks[index] = letter
             is_correct = True
             # all blanks are filled
-            if "".join(blanks) == chosen_word:
-                print("".join(blanks))
-                print("Game over, You Won")
         index += 1
+    if is_correct:
+        score += random.randint(10, 20)
+        right_attempts += 1
+        if "".join(blanks) == chosen_word:
+            print("".join(blanks))
+            print("Game over, You Won")
+            show_result()
 
-    if not is_correct:
+    else:
+        score -= random.randint(0, 10)
+        wrong_attempts += 1
         # loose a life
         print(stages[lives - 1])
         lives -= 1
@@ -117,3 +136,4 @@ while "".join(blanks) != chosen_word and lives > 0:
         # have they run out of lives
         if lives == 0:
             print("Game Over")
+            show_result()
