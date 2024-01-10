b76baf8839c29f9d279e5528fd8f160c4bead282
Manoj Kumar <ms8460149@gmail.com>
2024-01-10 08:41:40 +0530

    day8 of #100daysofpython

---
+++ ceaser-cipher/main.py
@@ -0,0 +1,86 @@
+logo = (
+    """           
+ ,adPPYba, ,adPPYYba,  ,adPPYba, ,adPPYba, ,adPPYYba, 8b,dPPYba,  
+a8"     "" ""     `Y8 a8P_____88 I8[    "" ""     `Y8 88P'   "Y8  
+8b         ,adPPPPP88 8PP"""
+    """"  `"Y8ba,  ,adPPPPP88 88          
+"8a,   ,aa 88,    ,88 "8b,   ,aa aa    ]8I 88,    ,88 88          
+ `"Ybbd8"' `"8bbdP"Y8  `"Ybbd8"' `"YbbdP"' `"8bbdP"Y8 88   
+            88             88                                 
+           ""             88                                 
+                          88                                 
+ ,adPPYba, 88 8b,dPPYba,  88,dPPYba,   ,adPPYba, 8b,dPPYba,  
+a8"     "" 88 88P'    "8a 88P'    "8a a8P_____88 88P'   "Y8  
+8b         88 88       d8 88       88 8PP"""
+    """" 88          
+"8a,   ,aa 88 88b,   ,a8" 88       88 "8b,   ,aa 88          
+ `"Ybbd8"' 88 88`YbbdP"'  88       88  `"Ybbd8"' 88          
+              88                                             
+              88           
+"""
+)
+alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+]
+
+print(logo)
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(text, shift):
+    cipher_text = ""
+    for char in text:
+        char_index = alphabet.index(char)
+        cipher_char_index = char_index + shift
+        if cipher_char_index >= 26:
+            cipher_char_index -= 26
+        cipher_text += alphabet[cipher_char_index]
+    print(f"The encoded text is {cipher_text}")
+
+
+def decrypt(text, shift):
+    plain_text = ""
+    for char in text:
+        char_index = alphabet.index(char)
+        plain_char_index = char_index - shift
+        plain_text += alphabet[plain_char_index]
+    print(f"The decoded text is {plain_text}")
+
+
+def caeser(text, shift, direction):
+    if direction == "encode":
+        encrypt(text, shift)
+    elif direction == "decode":
+        decrypt(text, shift)
+    else:
+        print("Invalid direction")
+
+
+caeser(text, shift, direction)
