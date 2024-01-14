2e85b5605650fbd8a0a9c68cbceb39bd0fa22eff
Manoj Kumar <ms8460149@gmail.com>
2024-01-14 10:55:33 +0530

    day12 of #100daysofcode

---
+++ guess-the-number/day12.py
@@ -0,0 +1,12 @@
+# Scope
+
+enemies = 1
+
+
+def increase_enemies():
+    enemies = 2
+    print(enemies)
+
+
+increase_enemies()
+print(enemies)
---
+++ guess-the-number/main.py
@@ -0,0 +1,28 @@
+from art import logo
+import random
+
+print(logo)
+print("Welcome to the guess the number game.")
+target = random.randint(1, 100)
+print("I'm thinking of a number between 1 and 100 :")
+diffiucltilevel = input(
+    "Choose your game difficulti level. Type 'easy' or 'hard': "
+).lower()
+if diffiucltilevel == "easy":
+    number_of_chances = 10
+else:
+    number_of_chances = 5
+is_game_over = False
+while not is_game_over and number_of_chances > 0:
+    print(f"You have {number_of_chances} attempts remaining to guess the number.")
+    user_guess = int(input("Make a guess: "))
+    number_of_chances -= 1
+    if user_guess > target:
+        print("Too high")
+    elif user_guess < target:
+        print("To low")
+    else:
+        print("Won")
+        is_game_over = True
+    if not is_game_over:
+        print("Guess again.")
