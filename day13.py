dd6a76ec32e1c3229a0a3ac0fcc5911656d2c8bb
Manoj Kumar <ms8460149@gmail.com>
2024-01-15 13:49:49 +0530

    debuged the problems using debugger

--- day13.py
+++ day13.py
@@ -15,15 +15,15 @@
 
 # # Play Computer
 # year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
-#   print("You are a millenial.")
+# if 1980 < year < 1994:
+#     print("You are a millennial.")
 # elif year >= 1994:
-#   print("You are a Gen Z.")
+#     print("You are a Gen Z.")
 
 # # Fix the Errors
 # age = int(input("How old are you?"))
 # if age > 18:
-#   print(f"You can drive at age {age}.")
+#     print(f"You can drive at age {age}.")
 
 # #Print is Your Friend
 # pages = 0
@@ -35,12 +35,12 @@
 # print(total_words)
 
 # #Use a Debugger
-def mutate(a_list):
-    b_list = []
-    for item in a_list:
-        new_item = item * 2
-        b_list.append(new_item)
-    print(b_list)
-
-
-mutate([1, 2, 3, 5, 8, 13])
+# def mutate(a_list):
+#     b_list = []
+#     for item in a_list:
+#         new_item = item * 2
+#         b_list.append(new_item)
+#     print(b_list)
+#
+#
+# mutate([1, 2, 3, 5, 8, 13])
