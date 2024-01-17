8cfa46be8493ddae8d2d6daee6eeed45829d3cc2
Manoj Kumar <ms8460149@gmail.com>
2024-01-17 08:38:14 +0530

    day15 of #100daysofcode

---
+++ day15/day15.py
@@ -0,0 +1,14 @@
+# Object Oriented Programming
+
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+#
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+# my_screen = Screen()
+#
+# print(my_screen.window_height())
+# my_screen.exitonclick()
+
