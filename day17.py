990d4d11a6b266d440a53233aea2a2c0f33b07ca
Manoj Kumar <ms8460149@gmail.com>
2024-01-19 10:13:07 +0530

    day17 of #100daysofcode

---
+++ day17.py
@@ -0,0 +1,72 @@
+import random
+# from turtle import
+import turtle as t
+
+tim = t.Turtle()
+
+t.colormode(255)
+
+tim.shape("turtle")
+tim.speed("fastest")
+# tim.color("red")
+# for _ in range(4):
+#     tim.forward(100)
+#     tim.left(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# tim.forward(100)
+# tim.right(90)
+# draw dashed line
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
+colors = ["red", "orange", "yellow", "green", "brown", "yellow"]
+
+
+#
+#
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+#
+#
+# for shape_side in range(3, 11):
+#     random_color = random.choice(colors)
+#     tim.color(random_color)
+#     draw_shape(shape_side)
+# directions = [0, 90, 180, 270]
+
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
+# tim.forward(100)
+# for _ in range(200):
+#     color = random_color()
+#     tim.color(color)
+#     tim.pensize(random.randint(9, 15))
+#     tim.speed(random.randint(4, 9))
+#     tim.forward(30)
+#     tim.setheading(random.choice(directions))
+
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
+
+
+draw_spirograph(10)
+
+screen = t.Screen()
+screen.exitonclick()
---
+++ hirst-painting/main.py
@@ -0,0 +1,59 @@
+import random
+
+# import colorgram
+import turtle as t
+import random
+
+tim = t.Turtle()
+t.colormode(255)
+tim.penup()
+tim.hideturtle()
+# colors = colorgram.extract("image.jpeg", 6)
+#
+# rgb_colors = []
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     rgb_colors.append((r, g, b))
+#
+# print(rgb_colors)
+
+color_list = [(211, 210, 210), (189, 167, 121), (57, 90, 111), (113, 43, 35), (163, 89, 64), (210, 212, 214)]
+
+# tim.home()
+#
+#
+# def draw_row():
+#     for num in range(10):
+#         random_color = random.choice(color_list)
+#         tim.dot(20, random_color)
+#         tim.penup()
+#         tim.forward(50)
+#
+#
+# for row in range(11):
+#     draw_row()
+#     tim.sety(row * 50)
+#     tim.setx(0)
+
+tim.setheading(220)
+
+tim.forward(300)
+tim.setheading(0)
+num_of_dotes = 100
+tim.speed("fastest")
+for num_count in range(1, num_of_dotes + 1):
+    tim.dot(20, random.choice(color_list))
+    tim.forward(50)
+
+    if num_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+screen = t.Screen()
+screen.exitonclick()
