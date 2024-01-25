d27254dc1ffd10558a4b2b51969e1508d5d518ad
Manoj Kumar <ms8460149@gmail.com>
2024-01-25 09:47:40 +0530

    day23 of #100daysofcode

---
+++ day23/main.py
@@ -0,0 +1,59 @@
+import pandas
+
+# import csv
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
+#     for row in data:
+#         if row[1] != "temp":
+#             new_temperature = int(row[1])
+#             temperatures.append(new_temperature)
+#         print(row)
+#     print(temperatures)
+
+# data = pandas.read_csv("weather_data.csv")
+# print(type(data))
+# print(type(data["temp"]))
+# data_dict = data.to_dict()
+# print(data_dict)
+# temperature = data["temp"]
+# temperature_list = temperature.to_list()
+# avg_temperature = sum(temperature_list) / len(temperature_list)
+# print(temperature_list, avg_temperature)
+# average = temperature.mean()
+# max_temp = temperature.max()
+# print(average, max_temp, temperature.tolist())
+# print(data[data["temp"] == data.temp.max()])
+# print(data[data.day == "Monday"])
+# print(data[data.condition == "Sunny"])
+
+# monday = data[data.day == "Monday"]
+# monday_temp = monday.temp[0]
+# monday_temp_f = monday_temp * 9/5 + 32
+# print(monday_temp_f)
+
+# Construct a Dataframe
+# data_dict = {
+#     "name": ["Manoj", "Monu", "Monika"],
+#     "age": [21, 20, 20]
+# }
+#
+# data = pandas.DataFrame(data_dict)
+# print(data)
+# data.to_csv("new_data.csv")
+fur_count = {}
+data = pandas.read_csv("squirrel.csv")
+gray_squirrel_count = len(data[data["Primary Fur Color"] == "Gray"])
+black_squirrel_count = len(data[data["Primary Fur Color"] == "Black"])
+cinnamon_squirrel_count = len(data[data["Primary Fur Color"] == "Cinnamon"])
+data_dict = {
+    "Fur Color": ["Gray", "Black", "Cinnamon"],
+    "Count": [gray_squirrel_count, black_squirrel_count, cinnamon_squirrel_count],
+}
+
+print(data_dict)
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_data.csv")
---
+++ us-game/main.py
@@ -0,0 +1,39 @@
+import turtle
+import pandas
+
+IMAGE = "./blank_states_img.gif"
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+
+screen.addshape(IMAGE)
+turtle.shape(IMAGE)
+
+data = pandas.read_csv("./50_states.csv")
+all_list = data.state.to_list()
+guessed_list = []
+is_game_on = True
+score = 0
+while len(guessed_list) <= 50:
+    user_answer = screen.textinput(
+        title=f"{score}/50 Guess the state", prompt="What's another state of USA? "
+    ).title()
+    if user_answer == "Exit":
+        missed_states = []
+        for state in all_list:
+            if state not in guessed_list:
+                missed_states.append(state)
+        df = pandas.DataFrame(missed_states)
+        df.to_csv("states_to_learn.csv")
+        break
+    if user_answer in all_list:
+        state_data = data[data.state == user_answer]
+        guessed_list.append(user_answer)
+        score += 1
+        state_name = state_data.state.item()
+        state_pos_x = int(state_data.x.iloc[0])
+        state_pos_y = int(state_data.y.iloc[0])
+        tim = turtle.Turtle()
+        tim.shape("circle")
+        tim.penup()
+        tim.goto(state_pos_x, state_pos_y)
+        tim.write(state_name, align="center", font=("Courier", 12, "normal"))
