c0454c2d2544ed48bd03d11f52d344ff45bcec45
Manoj Kumar <ms8460149@gmail.com>
2024-01-26 17:50:16 +0530

    renamed data_dict to phonetic_dict

--- nato-project/main.py
+++ nato-project/main.py
@@ -2,8 +2,8 @@ import pandas
 
 data = pandas.read_csv("./nato_phonetic_alphabet.csv")
 
-data_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 word = input("Enter a word: ").upper()
-code_words = [data_dict[letter] for letter in word]
+code_words = [phonetic_dict[letter] for letter in word]
 print(code_words)
