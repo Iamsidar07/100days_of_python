afe1f37a03d2ba6933b5db3ef5164722fe1223fa
Manoj Kumar <ms8460149@gmail.com>
2024-01-31 19:05:34 +0530

    formatted code

--- flashy/main.py
+++ flashy/main.py
@@ -12,6 +12,7 @@ finally:
     to_learn = data.to_dict(orient="records")
 current_card = {}
 
+
 def next_card():
     global current_card, flip_timer
     window.after_cancel(flip_timer)
@@ -21,7 +22,7 @@ def next_card():
     canvas.itemconfig(card_title, text="French", fill="black")
     canvas.itemconfig(card_word, text=french_word, fill="black")
     flip_timer = window.after(TIMER, func=flip_card)
-    
+
 
 def flip_card():
     global current_card
@@ -30,17 +31,18 @@ def flip_card():
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=english_word, fill="white")
 
+
 def is_known():
     to_learn.remove(current_card)
     df = pd.DataFrame(to_learn)
     df.to_csv("./data/words_to_learn.csv", index=False)
     next_card()
 
+
 window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 flip_timer = window.after(TIMER, func=flip_card)
-
 
 canvas = Canvas(width=800, height=526, highlightthickness=0, bg=BACKGROUND_COLOR)
 canvas.grid(column=0, row=0, columnspan=2)
@@ -49,10 +51,10 @@ card_back_img = PhotoImage(file="./images/card_back.png")
 card_front_img = PhotoImage(file="./images/card_front.png")
 card_background = canvas.create_image(400, 263, image=card_front_img)
 
-card_title = canvas.create_text(400, 153, text="Text", font=("Cousine", 30, "italic"))
-card_word = canvas.create_text(400, 260, text="Word", font=("Cousine", 34, "bold"))
+card_title = canvas.create_text(400, 153, text="Text", font=("Monolisa", 30, "italic"))
+card_word = canvas.create_text(400, 260, text="Word", font=("Monolisa", 34, "bold"))
 
-wrong_img = PhotoImage(file="./images/wrong.png",)
+wrong_img = PhotoImage(file="./images/wrong.png", )
 unknown_btn = Button(image=wrong_img, highlightthickness=0, command=next_card)
 unknown_btn.grid(row=1, column=0)
 
