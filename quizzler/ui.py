0dee7be61e66fbacc590265eec3134de67b7f6cb
Manoj Kumar <ms8460149@gmail.com>
2024-02-01 17:19:14 +0530

    formatted code

--- quizzler/ui.py
+++ quizzler/ui.py
@@ -5,27 +5,38 @@ THEME_COLOR = "#375362"
 
 
 class QuizInterface:
-
     def __init__(self, quiz_brain: QuizBrain):
         self.quiz = quiz_brain
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(bg=THEME_COLOR, padx=20, pady=20)
 
-        self.score_label = Label(text="Score=0", fg="white", font=("Monolisa", 12, "normal"), bg=THEME_COLOR)
+        self.score_label = Label(
+            text="Score=0", fg="white", font=("Monolisa", 12, "normal"), bg=THEME_COLOR
+        )
         self.score_label.grid(row=0, column=1)
 
         self.canvas = Canvas(width=300, height=250)
-        self.question_text = self.canvas.create_text(150, 125, width=280, fill=THEME_COLOR, text="Hello World",
-                                                     font=("Arial", 20, "italic"))
+        self.question_text = self.canvas.create_text(
+            150,
+            125,
+            width=280,
+            fill=THEME_COLOR,
+            text="Hello World",
+            font=("Arial", 20, "italic"),
+        )
         self.canvas.grid(row=1, column=0, columnspan=2, pady=50)
 
         right_img = PhotoImage(file="images/true.png")
-        self.right_btn = Button(image=right_img, highlightthickness=0, command=self.right_pressed)
+        self.right_btn = Button(
+            image=right_img, highlightthickness=0, command=self.right_pressed
+        )
         self.right_btn.grid(row=2, column=0)
 
         wrong_img = PhotoImage(file="images/false.png")
-        self.wrong_btn = Button(image=wrong_img, highlightthickness=0, command=self.wrong_pressed)
+        self.wrong_btn = Button(
+            image=wrong_img, highlightthickness=0, command=self.wrong_pressed
+        )
         self.wrong_btn.grid(row=2, column=1)
 
         self.get_next_question()
@@ -38,8 +49,11 @@ class QuizInterface:
             q_text = self.quiz.next_question()
             self.canvas.itemconfig(self.question_text, text=q_text)
         else:
-            self.canvas.itemconfig(self.question_text, text=f"You've completed the quiz.\nYour final score was: "
-                                                            f"{self.quiz.score}/{self.quiz.question_number}")
+            self.canvas.itemconfig(
+                self.question_text,
+                text=f"You've completed the quiz.\nYour final score was: "
+                f"{self.quiz.score}/{self.quiz.question_number}",
+            )
             self.right_btn.config(state="disabled")
             self.wrong_btn.config(state="disabled")
 
@@ -52,10 +66,8 @@ class QuizInterface:
         self.give_feedback(is_correct)
 
     def give_feedback(self, is_correct: bool):
-        
         if is_correct:
             self.canvas.config(bg="green")
         else:
             self.canvas.config(bg="red")
         self.window.after(1000, func=self.get_next_question)
-
