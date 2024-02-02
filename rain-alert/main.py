df3a1b7e07b9f9bb717bc821320394967d3201c7
Manoj Kumar <ms8460149@gmail.com>
2024-02-02 11:46:55 +0530

    day31 of #100daysofcode

---
+++ rain-alert/main.py
@@ -0,0 +1,39 @@
+import requests
+from twilio.rest import Client
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+URL = "https://api.openweathermap.org/data/2.5/forecast"
+MY_LAT = 23.116240
+MY_LONG = 83.195923
+API_KEY = os.getenv("API_KEY")
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+parameters = {
+    "lat": MY_LAT, 
+    "lon": MY_LONG, 
+    "cnt": 4, 
+    "appid": API_KEY
+    }
+
+response = requests.get(url=URL, params=parameters)
+response.raise_for_status()
+data = response.json()
+will_rain = False
+for weather in data["list"]:
+    weather_id = int(weather["weather"][0]["id"])
+    if weather_id < 700:
+        print("bring umbrella")
+        will_rain = True
+
+if will_rain:
+    # Send text sms0
+    message = client.messages.create(
+        from_="+14783129798",
+        to="+917879189091",
+        body="Take umbrella☂️ with you. It's rainny today!",
+    )
+    print(message)
