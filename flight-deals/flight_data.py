93d60076ff66fa991a49c3db9b3f3eb885489d23
Manoj Kumar <ms8460149@gmail.com>
2024-02-05 09:51:15 +0530

    feat:flight-data

--- flight-deals/flight_data.py
+++ flight-deals/flight_data.py
@@ -1,3 +1,14 @@
+import requests
+GOOGLE_SHEET_ENDPOINT = "https://api.sheety.co/2d98cf2c677367f48c200f8bb7b0cbd1/flightDeals/prices"
+GOOGLE_SHEET_TOKEN = "asdfjklowncaowljkamgoincoaieowjdkl"
+headers = {
+    "Authorization": f"Bearer {GOOGLE_SHEET_TOKEN}"
+}
+
 class FlightData:
     #This class is responsible for structuring the flight data.
-    pass
+    def __init__(self):
+        response = requests.get(url=GOOGLE_SHEET_ENDPOINT, headers=headers)
+        response.raise_for_status()
+        self.data = response.json()["prices"]
+        
--- flight-deals/main.py
+++ flight-deals/main.py
@@ -1,1 +1,12 @@
 #This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from flight_search import FlightSearch
+from flight_data import FlightData
+from data_manager import DataManager
+from notification_manager import NotificationManager
+
+fligh_search = FlightSearch()
+flight_data = FlightData()
+data_manager = DataManager()
+notification_manager = NotificationManager()
+
+print(flight_data.data)
