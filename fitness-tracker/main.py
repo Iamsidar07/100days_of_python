b24d52c284ccf7549ade62e0611a1ae3ffe03ebc
Manoj Kumar <ms8460149@gmail.com>
2024-02-05 08:56:15 +0530

    day34 of #100daysofcode

--- fitness-tracker/main.py
+++ fitness-tracker/main.py
@@ -1,2 +1,59 @@
-APP_ID = "062020a1"
-API_KEY = "7c39fbae731cda85570678f8c97da601"
+import requests
+from datetime import datetime
+import os
+
+APP_ID = os.environ.get("APP_ID")
+API_KEY = os.environ.get("API_KEY")
+GOOGLE_SHEET_TOKEN = os.environ.get("GOOGLE_SHEET_TOKEN")
+GOOGLE_SHEETY_ENDPOINT = os.environ.get("GOOGLE_SHEETY_ENDPOINT")
+
+NUTRITIONIX_BASE_URL = "https://trackapi.nutritionix.com"
+NUTRITIONIX_ENDPOINT = "/v2/natural/exercise"
+
+
+headers = {
+    "Content-Type": "application/json", 
+    "x-app-id": APP_ID, 
+    "x-app-key": API_KEY
+}
+
+request_body = {
+    "query": input("Tell me what exercies you did: ")
+}
+
+response = requests.post(
+    url=f"{NUTRITIONIX_BASE_URL}{NUTRITIONIX_ENDPOINT}",
+    json=request_body,
+    headers=headers,
+)
+response.raise_for_status()
+data = response.json()["exercises"][0]
+exercise = data["name"]
+duration = data["duration_min"]
+calories = data["nf_calories"]
+
+now = datetime.now().strftime("%d/%m/%Y")
+current_time = datetime.now().strftime("%H:%M:%S")
+
+google_sheety_headers = {
+    "Authorization": f"Bearer {GOOGLE_SHEET_TOKEN}"
+}
+
+google_sheety_req_body = {
+    "workout": {
+        "date": now,
+        "time": current_time,
+        "exercise": exercise.title(),
+        "duration": duration,
+        "calories": calories,
+    }
+}
+
+response = requests.post(
+    url=GOOGLE_SHEETY_ENDPOINT,
+    json=google_sheety_req_body,
+    headers=google_sheety_headers,
+)
+response.raise_for_status()
+data = response.json()
+print(data)
