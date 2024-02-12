991c53cbbf0194cf66ee119ed724760ce33d1375
Manoj Kumar <ms8460149@gmail.com>
2024-02-12 12:28:23 +0530

    style: format code

--- musical-time-machine/main.py
+++ musical-time-machine/main.py
@@ -7,17 +7,21 @@ import os
 SPOTIFY_CLIENT_ID = os.environ.get(["SPOTIFY_CLIENT_ID"])
 SPOTIFY_CLIENT_SECRET = os.environ.get(["SPOTIFY_CLIENT_SECRET"])
 
-sp = spotipy.Spotify(auth_manager=SpotifyOAuth(
-    client_id=SPOTIFY_CLIENT_ID,
-    client_secret=SPOTIFY_CLIENT_SECRET,
-    redirect_uri="http://example.com",
-    scope="playlist-modify-private",
-    username="Manoj Kumar",
-    cache_path="token.txt",
-    show_dialog=True
-))
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        client_id=SPOTIFY_CLIENT_ID,
+        client_secret=SPOTIFY_CLIENT_SECRET,
+        redirect_uri="http://example.com",
+        scope="playlist-modify-private",
+        username="Manoj Kumar",
+        cache_path="token.txt",
+        show_dialog=True,
+    )
+)
 
-desired_date = input("which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+desired_date = input(
+    "which year do you want to travel to? Type the date in this format YYYY-MM-DD: "
+)
 URL = f"https://www.billboard.com/charts/hot-100/{desired_date}"
 
 response = requests.get(URL)
@@ -30,7 +34,9 @@ USER_ID = sp.current_user()["id"]
 
 song_uris = []
 for song in song_titles:
-    result = sp.search(q=f"track:{song} year:{desired_date.split('-')[0]}", type="track")
+    result = sp.search(
+        q=f"track:{song} year:{desired_date.split('-')[0]}", type="track"
+    )
     try:
         uri = result["tracks"]["items"][0]["uri"]
         song_uris.append(uri)
@@ -41,15 +47,9 @@ PLAYLIST_NAME = f"{desired_date} Billboard 100"
 
 
 playlist = sp.user_playlist_create(
-    name=PLAYLIST_NAME,
-    user=USER_ID,
-    public=False,
-    description="100 billboard"
+    name=PLAYLIST_NAME, user=USER_ID, public=False, description="100 billboard"
 )
 
 PLAYLIST_ID = playlist["id"]
 
-result = sp.playlist_add_items(
-    playlist_id=PLAYLIST_ID,
-    items=song_uris
-)
+result = sp.playlist_add_items(playlist_id=PLAYLIST_ID, items=song_uris)
