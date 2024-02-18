fdfc32005070c232825b19c0c1ed3e9b3ee2cc9e
Manoj Kumar <ms8460149@gmail.com>
2024-02-18 17:14:03 +0530

    feat: advance decorators exercise

---
+++ hello-flask/advance_decorators.py
@@ -0,0 +1,12 @@
+def logging_decorator(func):
+    def wrapper_function(*args):
+        print(f"You called {func.__name__}{args}")
+        result = func(args[0], args[1], args[2])
+        print(f"It returned: {result}")
+    return wrapper_function
+
+@logging_decorator
+def a_function(a, b, c):
+  return a * b * c
+
+a_function(1, 2, 3)
