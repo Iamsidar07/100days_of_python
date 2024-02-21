b182e6f661783c44b60955c6aea0a87a4705f283
Manoj Kumar <ms8460149@gmail.com>
2024-02-21 09:55:08 +0530

    feat: tweet news and can interact with tweets

--- OctagonInsider/main.py
+++ OctagonInsider/main.py
@@ -2,5 +2,6 @@ from octagon_insider import OctagonInsider
 
 octagon_insider = OctagonInsider()
 octagon_insider.get_news()
-print(octagon_insider.all_news)
-# octagon_insider.tweet_news()
+octagon_insider.login_with_twitter()
+octagon_insider.tweet_news()
+octagon_insider.interact_with_tweets()
--- OctagonInsider/octagon_insider.py
+++ OctagonInsider/octagon_insider.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 import requests
@@ -13,8 +14,6 @@ from insider_news import InsiderNews
 
 USERNAME = ""
 PASSWORD = ""
-PROMISED_DOWN = 150
-PROMISED_UP = 10
 
 
 class OctagonInsider:
@@ -41,8 +40,8 @@ class OctagonInsider:
             all_news.append(new_news)
         self.all_news = all_news
 
-    def tweet_news(self):
-        time.sleep(5)
+    def login_with_twitter(self):
+        print("logging ...")
         self.driver.get("https://twitter.com/i/flow/login")
         time.sleep(3)
         WebDriverWait(self.driver, 15).until(
@@ -60,6 +59,9 @@ class OctagonInsider:
         password.send_keys(PASSWORD, Keys.ENTER)
         time.sleep(20)
 
+    def tweet_news(self):
+        print("Tweeting...")
+        news = random.choice(self.all_news)
         compose_btn = self.driver.find_element(
             By.CSS_SELECTOR, "a[href='/compose/post']"
         )
@@ -73,12 +75,48 @@ class OctagonInsider:
             "2]/div/div/div/div/label/div[1]/div/div/div/div/div/div["
             "2]/div",
         )
-        for news in self.all_news:
-            tweet = f"{news.title}\n\n{news.url}".encode("utf-8")
-            tweet_input.send_keys(tweet)
-            time.sleep(3)
-            tweet_btn = self.driver.find_element(
-                By.CSS_SELECTOR, "div[data-testid='tweetButton']"
-            )
-            tweet_btn.click()
-            time.sleep(2)
+        tweet = f"{news.title}\n\n{news.url}\n#OctagonInsider #ufc #mma "
+        print(tweet)
+        tweet_input.send_keys(tweet)
+        time.sleep(3)
+        tweet_btn = self.driver.find_element(
+            By.CSS_SELECTOR, "div[data-testid='tweetButton']"
+        )
+        tweet_btn.click()
+        time.sleep(15)
+
+    def interact_with_tweets(self):
+        print("Interacting...")
+        like_btns = self.driver.find_elements(
+            By.CSS_SELECTOR, "div[data-testid='like']"
+        )
+        retweet_btns = self.driver.find_elements(
+            By.CSS_SELECTOR, "div[data-testid='retweet']"
+        )
+        more_btns = self.driver.find_elements(
+            By.CSS_SELECTOR, "div[data-testid='caret']"
+        )
+
+        for i in range(len(like_btns)):
+            random_chance = random.randint(1, 3)
+            if random_chance == 1:
+                like_btns[i].click()
+                print("liked")
+            elif random_chance == 2:
+                more_btns[i].click()
+                time.sleep(2)
+                follow_btn = self.driver.find_element(
+                    By.CSS_SELECTOR, "div[data-testid='Dropdown'] > div:nth-child(2)"
+                )
+                if "Follow" in follow_btn.text:
+                    follow_btn.click()
+                    print("Followed")
+                time.sleep(1)
+
+            else:
+                retweet_btns[i].click()
+                retweet_confirm = self.driver.find_element(
+                    By.CSS_SELECTOR, "div[data-testid='retweetConfirm']"
+                )
+                retweet_confirm.click()
+                print("retweet")
