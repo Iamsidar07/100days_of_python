3b4b3b75a727ee38b39508daf68e53a83325abc3
iamsidar07 <ms8460149@gmail.com>
2024-02-23 08:59:53 +0530

    feat: make the website work

--- web-development-projects/virtual-bookshelf/main.py
+++ web-development-projects/virtual-bookshelf/main.py
@@ -1,33 +1,31 @@
 from flask import Flask, render_template, request, redirect, url_for
-
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
+from book import Book
 
 app = Flask(__name__)
 
 all_books = []
 
+print(all_books)
+
 
 @app.route('/')
 def home():
-    pass
+    return render_template("index.html", books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        new_book = Book(
+            title=request.form.get("title"),
+            author=request.form.get("author"),
+            rating=request.form.get("rating")
+        )
+        all_books.append(new_book)
+        return redirect(url_for('home'))
+    else:
+        return render_template("add.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
