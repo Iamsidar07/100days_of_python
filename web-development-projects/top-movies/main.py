d4106a98e3d46a7d109ea40bebe485a607b76b09
iamsidar07 <ms8460149@gmail.com>
2024-02-24 16:54:17 +0530

    feat: rest full cafe api

--- web-development-projects/top-movies/main.py
+++ web-development-projects/top-movies/main.py
@@ -18,7 +18,9 @@ class Base(DeclarativeBase):
 
 
 class RateMovieForm(FlaskForm):
-    rating = StringField(label="Your rating out of 10 eg. 7.5", validators=[DataRequired()])
+    rating = StringField(
+        label="Your rating out of 10 eg. 7.5", validators=[DataRequired()]
+    )
     review = StringField(label="Your review", validators=[DataRequired()])
     submit = SubmitField(label="Done")
 
@@ -29,7 +31,7 @@ class AddMovieForm(FlaskForm):
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///top-movies.db"
 Bootstrap5(app)
 
@@ -58,8 +60,8 @@ with app.app_context():
     #     title="Phone Booth",
     #     year=2002,
     #     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an "
-    #                 "extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with "
-    #                 "the caller leads to a jaw-dropping climax.",
+    #                 "extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's"
+    #                 " negotiation with the caller leads to a jaw-dropping climax.",
     #     rating=7.3,
     #     ranking=10,
     #     review="My favourite character was the caller.",
@@ -71,7 +73,9 @@ with app.app_context():
 
 @app.route("/")
 def home():
-    all_movies = db.session.execute(db.select(Movie).order_by(Movie.rating)).scalars().all()
+    all_movies = (
+        db.session.execute(db.select(Movie).order_by(Movie.rating)).scalars().all()
+    )
     for i in range(len(all_movies)):
         all_movies[i].ranking = len(all_movies) - i
     db.session.commit()
@@ -106,11 +110,7 @@ def add_movie():
     form = AddMovieForm()
     if form.validate_on_submit():
         movie_title = form.title.data
-        params = {
-            "apikey": OMDB_API_KEY,
-            "s": movie_title,
-            "r": "json"
-        }
+        params = {"apikey": OMDB_API_KEY, "s": movie_title, "r": "json"}
         response = requests.get(url=OMDB_ENDPOINT, params=params)
         response.raise_for_status()
         search_results = response.json()["Search"]
@@ -121,11 +121,7 @@ def add_movie():
 @app.route("/get-movie-detail")
 def get_movie_detail():
     imdb_id = request.args.get("id")
-    params = {
-        "apikey": OMDB_API_KEY,
-        "i": imdb_id,
-        "r": "json"
-    }
+    params = {"apikey": OMDB_API_KEY, "i": imdb_id, "r": "json"}
     response = requests.get(url=OMDB_ENDPOINT, params=params)
     movie = response.json()
     new_movie = Movie(
@@ -142,5 +138,5 @@ def get_movie_detail():
     return redirect(url_for("edit", id=new_movie.id))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
